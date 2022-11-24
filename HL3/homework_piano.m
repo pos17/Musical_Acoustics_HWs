5b36ab32e9591ef832c69f39211ad2217b2dd499
pox17 <paos9904@gmail.com>
2022-11-24 18:21:18 +0100

    updated hl3

--- HL3/homework_piano.m
+++ HL3/homework_piano.m
@@ -19,26 +19,36 @@ clc
 % as C2.
 
 % Temporal sampling parameters
-Fs = 16000 % [Hz]
-time_L = 8; % [s]
+Fs = 16000;             % [Hz] Following the paper
+%Fs = 4*44100;             % [Hz] Following the slides
+T = 1/Fs;
+dur = 8;             % [s]
+N = dur*Fs
+%t = linspace(0, Fs, N);
 
 
-f_1 = 65.4; %[Hz] % Fundamental note
 % Boundary          
 
+z_l = 1e+20;
+z_b = 1000;
 
 % String parameters
+f_1 = 65.4;             % [Hz]  Fundamental note
 str_L = 1.92;           % [m]   string length
+M_s = 35e-3;            % [Kg]  String mass
+T_e = 750;              % [N]   string tension   
 b_1 = 0.5;              %       air damping coefficient
 b_2 = 6.25e-9;          %       string internal friction coefficient
-w = 0.2;                %       width of the hammer spatial window 𝑔
-Vh_0 = 2.5;             % [m/s] initial hammer velocity
 epsilon = 7.5 * 10^(-6);%       string stiffness parameter
 k = epsilon;            %       string stiffness coefficient
 
+rho = M_s / str_L;      % [Kg/m]string linear density    
+c = sqrt(T_e/rho);      % [m/s] string propagation velocity
 
 
 % Spatial sampling parameters
+%M = X = T * c       % maximum value which allows CFL conditions
+%M = 521;            % fixed by the slides
 
 
 
@@ -46,34 +56,161 @@ k = epsilon;            %       string stiffness coefficient
 
 
 % Number of maximum spatial steps
-gamma = Fs/2*f_1;
-N_max = sqrt((sqrt(-1+(1+(16*epsilon*gamma^2))))/(8*epsilon))
+gamma = Fs/(2*f_1);
+M_max = sqrt((-1+sqrt(1+16*epsilon*gamma^2))/(8*epsilon));
+%X_max = sqrt((1/2)*(c^2*T^2 + 4*b_2*T + sqrt(((c^2)*T^2 + 4*b_2*T)^2 + 16*k^2*T^2)));
+M = floor(M_max);
+X = str_L/M;
 
 % Integer values
+lambda = c*T/X;
+mu = k^2/(c^2*X^2);
+v= (2*b_2*T)/(X^2);
+
+
 % Spatial sampling
 
 
 
 % FD parameters
+a_1 = (-lambda^2*mu)/(1+b_1*T);
+a_2 = (lambda^2 + (4*lambda^2*mu) + v)/(1+b_1*T);
+a_3 = (2-2*lambda^2-6*lambda^2*mu-2*v)/(1+b_1*T);
+a_4 = (-1+b_1*T+2*v)/(1+b_1*T);
+a_5 = (-v)/(1+b_1*T);
+a_F = (T^2/rho)/(1+b_1*T);
 
 % Hammer parameters
-
+M_H = 4.9e-3;           % [Kg]  mass of the hammer
+w = 0.2;                %       width of the hammer spatial window 𝑔
+p = 2.3;
+Vh_0 = 2.5;             % [m/s] initial hammer velocity
+b_H = 1e-4;             % [1/s] fluid damping coefficient
+K = 4e+8;               %       hammer felt stiffness
+a=0.12;                  %       x_0/L relative position of hammer strike
+a_M = round(M*a);              %       relative hammer position in space samples
 % Hammer contact window definition
-
+w_M= round(w/X);
+g = hann(w_M);
 
 %PDE Coefficients:
 
 % Bridge boundary coefficients
+b_R1 = (2-2*lambda^2*mu-2*lambda^2)/(1+b_1*T+z_b*lambda);
+b_R2 = (4*lambda^2*mu+2*lambda^2)/(1+b_1*T+z_b*lambda);
+b_R3 = (-2*lambda^2*mu)/(1+b_1*T+z_b*lambda);
+b_R4 = (-1-b_1*T+z_b*lambda)/(1+b_1*T+z_b*lambda);
+b_RF = (T^2/rho)/(1+b_1*T+z_b*lambda);
 
 % Left hand (hinged string end) boundary coefficients
+b_L1 = (2-2*lambda^2*mu-2*lambda^2)/(1+b_1*T+z_l*lambda);
+b_L2 = (4*lambda^2*mu+2*lambda^2)/(1+b_1*T+z_l*lambda);
+b_L3 = (-2*lambda^2*mu)/(1+b_1*T+z_l*lambda);
+b_L4 = (-1-b_1*T+z_b*lambda)/(1+b_1*T+z_l*lambda);
+b_LF = (T^2/rho)/(1+b_1*T+z_l*lambda);
 
 % Hammer felt parameters
+
+d_1 = 2/(1+(b_H*T)/(2*M_H));
+d_2 =(-1 + (b_H*T)/(2*M_H))/(1 + (b_H*T)/(2*M_H));
+d_F = (-T^2/M_H)/(1+(b_H*T)/(2*M_H));
+
 
 %% Computation of the FD scheme
 % Initialization
 
+% y string displacement vector 
+y=zeros([N,M]);     % each row is a time instant considered. 
+                    % each column is a space fragment
+% F_H time vector
+F_H=zeros(N,1);     % each row is a time instant considered. 
+                    % each column is a space fragment
+
+F=zeros(N,M);      % 
+
+space = linspace(0, str_L, M);
+j_start = a_M-(round(w_M/2));
+disp(j_start)
+for i=1:N
+    for j= 1:size(g)
+        F(i,j+j_start) = g(j);
+    end
+
+end
+
+
+%%
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+plot(space/str_L, F(10,:), LineWidth=1.2);
+grid minor;
+% surf(F);
+disp(F(10,a_M));
+%%
+
+% eta hammer displacement vector 
+eta = zeros(N,1);
+eta(1) = 0;         % eta time= 0
+eta(2) = Vh_0*T; 
+
+%%
+
+
 % Computation loop
+
+
+
+for in=2:N-1          %   time loop
+        
+
+        % force time computation
+        F_H(in) = K* abs(eta(in)-y(in,a_M))^p;
+        % hammer displacement computation
+        if in>=2
+            eta(in+1)= d_1*eta(in)+d_2*eta(in-1)+d_F*F_H(in);
+        end 
+        if eta(in+1)>= y(in+1,a_M)
+            F(in+1,:)=F(in+1,:)*F_H(in+1);
+        else
+            F(in+1,:) = 0;
+        end
+    for im=1:M      %   space loop
+
+        % solving displacement boundary conditions
+        switch im
+            % m=0
+            case 1
+                y(in+1,im) = b_L1*y(in,im)+b_L2*y(in,im+1)+b_L3*y(in,im+2)...
+                    +b_L4*y(in-1,im)+b_LF*F(in,im);
+            % m=1
+            case 2
+                y(in+1,im) = a_1*(y(in,im+2)-y(in,im)+2*y(in,im-1))+a_2*(y(in,im+1)+y(in,im-1))+ ...
+                    +a_3*(y(in,im))+a_4*(y(in-1,im))+a_5*(y(in-1,im+1)+y(in-1,im-1))+a_F*F(in,im);
+            % m=M-1
+            case M-1
+                y(in+1,im) = a_1*(2*y(in,im+1)-y(in,im)+y(in,im-2))+a_2*(y(in,im+1)+y(in,im-1))+ ...
+                    +a_3*(y(in,im))+a_4*(y(in-1,im))+a_5*(y(in-1,im+1)+y(in-1,im-1))+a_F*F(in,im);
+            % m=M
+            case M
+                y(in+1,im)= b_R1*y(in,im)+b_R2*y(in,im-1)+b_R3*y(in,im-2)+b_R4*y(in-1,im)+b_RF*F(in,im);
+            otherwise
+                y(in+1,im)= a_1*(y(in,im+2)+y(in,im-2))+a_2*(y(in,im+1)+y(in,im-1))+a_3*y(in,im)+a_4*(y(in-1,im))+a_5*(y(in-1,im+1)+y(in-1,im-1))+a_F*(F(in,im));
+        end
+    end
+end
+%%
+f=linspace(0,Fs,N);
+freqs = abs(fft(y(:,a_M)));
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+grid minor;
+plot(f,freqs, LineWidth=1.2);
+xlim([0,Fs/2])
+
 %% Plot the displacement in time
+t=linspace(0, dur, N);
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+
+plot(t, y(:,a_M), LineWidth=1.2);
+
 
 %% Plot the synthesized signal play it and save it on the disk
 
