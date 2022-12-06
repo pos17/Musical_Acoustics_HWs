c39077ea985d0bb4db8ab4fb9779bc3b62537594
pox17 <paos9904@gmail.com>
2022-12-06 17:43:11 +0100

    added delivery folder with files and comments

---
+++ HL3/Delivery/GuitarDeliver/GuitarModeling.m
@@ -0,0 +1,192 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Modeling of musical instruments homework.                               %
+% Numerical simulation of a guitar                                        %
+% Physical model for a guitar exploiting electric analogs.                %
+%                                                                         %
+% Musical Acoustics course                                                %
+% Ostan Paolo                                                             %
+% Donà Stefano                                                            %
+% 2022                                                                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% NOTES:
+% in this file we didn't use the functions myFFT() and 
+% plotFFT_linearFreqScale() because we noticed that the fft returned by the
+% first function wasn't smooth enough in order to collect qualitative and
+% quantitave informations from it. Instead, the built-in fft of MATLAB gave
+% us the smoothness we needed. Moreover, given the high differences in
+% terms of order of magnintude of the different peaks we built our own
+% function FRF_Plot that is an easier modification of the second given
+% function, and it plots the magnitude in dB scale in order to understand
+% better the behavior of the system.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear;
+close all;
+clc;
+
+addpath('Functions')
+simulink_folder = './Simulink/';       % Simulink projects folder
+addpath(simulink_folder);
+
+if not(isfolder("plots"))
+    mkdir("plots")
+end
+
+
+%% Setup
+fs = 48000;                         % Sampling frequency
+signalLen = 5;                      % Signal length    
+t = linspace(0, signalLen, (signalLen*fs));  % Time axis
+
+
+%% SIMULATION OF IMPULSE RESPONSE
+% run the simulink simulation using the command sim (see doc sim).
+flag = 0; % commutation of switch to the impulse excitation
+sim('GuitarModel2'); 
+
+
+%% PLOTTING OF THE ADMITTANCE OF THE GUITAR BODY
+
+% The variable I contains non constant time intervals between samples.
+% Resample the data using resample function in order to obtain an equally
+% sampled signal I1
+I = resample(I_out, t);
+V = resample(V_out, t);
+
+% H is the admittance of the system
+H = fft(I.Data)./fft(V.Data);
+H = H(1:floor(length(H)/2));
+
+% frequency axis
+frq = linspace(0,fs/2, length(H));
+
+g = figure('Renderer', 'painters', 'Position', [100 100 1000 600]);
+FRF_Plot('H', H, frq, 2000, g);
+sgtitle("Admittance of the Guitar Body ")
+% saving plot
+saveas(gcf, ".\plots\GTR_Ex1_Admittance.png");
+
+% searching for resonances
+[pks, locs] = findpeaks(abs(H));
+resonances = frq(locs)';
+
+disp("resonances of the system");
+disp(resonances);
+
+
+%% SIMULATION OF HARMONIC EXCITATION
+flag = 2; % commutation of the switch to the damped squared signal
+sim('GuitarModel2'); 
+I = resample(I_out, t);
+
+
+%% PLOTTING THE 
+
+figure('Renderer', 'painters', 'Position', [100 100 1000 600])
+plot(I.Time, I.Data);
+grid minor
+xlabel('time [s]'); ylabel('current [A]');
+title('Velocity of top plate');
+% saving plot
+saveas(gcf, ".\plots\GTR_Ex1_VelTime.png");
+
+figure('Renderer', 'painters', 'Position', [100 100 1000 600])
+plot(I.Time, I.Data);
+grid minor
+xlabel('time [s]'); ylabel('current [A]');
+title('Velocity of top plate zoomed');
+xlim([0 0.1])
+% saving plot
+saveas(gcf, ".\plots\GTR_Ex1_VelTimeZoomed.png");
+
+
+%% Plot and play
+% Plot the signal frequency content as magnitude and phase
+S = fft(I.Data);
+S = S(1:floor(length(S)/2));
+
+h = figure('Renderer', 'painters', 'Position', [100 100 1000 600]);
+
+FRF_Plot('', H, frq, 2000, h);
+hold on
+FRF_Plot(' . ' , S, frq, 2000, h);
+legend(["admittance", "top plate vel"], Position=[0.790099999227524,0.47358333269755,0.115800001544952,0.060833334604899])
+sgtitle("Top Plate Velocity vs Admittance")
+% saving plot
+saveas(gcf, ".\plots\GTR_Ex1_AdmVSVel.png");
+
+
+%% LISTENING AND SAVING SOUNDWAVE
+
+fileName = 'GuitarModelNoString.wav';     % Audio file path
+
+% Normalize the signal
+soundWave = I.Data;
+soundWave = soundWave./max(abs(soundWave));
+
+sound(soundWave, fs);                           % Play the sound
+
+disp('Save file on disk...')                    % Save on disk
+audiowrite(fileName, soundWave, fs);
+
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                         %
+%                   SIMULATION OF BODY AND REAL STRING                    %
+%                                                                         %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Body and string Simulation
+% run the simulink simulation using the command sim (see doc sim).
+sim('GuitarModel3'); 
+
+% The variable I contains non constant time intervals between samples.
+% Resample the data using resample function in order to obtain an equally
+% sampled signal I_2
+
+I2 = resample(I_2, t);
+
+% Plot the resampled signal in time
+figure('Renderer', 'painters', 'Position', [100 100 1000 600]);
+plot(I2);
+xlabel('time [s]'); ylabel('current [A]')
+title('Velocity of top plate w/ string model')
+grid minor
+% saving plot
+saveas(gcf, ".\plots\GTR_Ex2_VelTime.png");
+
+% Normalize the signal 
+soundWave2 = I2.Data;
+soundWave2 = soundWave2./max(abs(soundWave2));
+
+
+%% PLOTTING FFT
+
+% Plot the signal frequency content as magnitude and phase
+S = fft(I2.Data); 
+S = S(1:floor(length(S)/2));
+jj = figure('Renderer', 'painters', 'Position', [100 100 1000 600]);
+frq = linspace(0,fs/2, length(S));
+FRF_Plot('Vel', S, frq, 5500, jj)
+sgtitle('Frequency response of the velocity w/ string model')
+allAxis=get(gcf, 'Children');
+
+f0 = 329.5;
+xline(f0, 'k--', 'LineWidth', 2,'Parent', allAxis(3));
+xline([f0*5 f0*10 f0*15], 'r--', 'LineWidth', 2,'Parent', allAxis(3));
+
+% saving plot
+saveas(gcf, ".\plots\GTR_Ex2_VelFreq.png");
+
+
+%% LISTENING AND PRINTING FILE
+
+fileName = 'GuitarModelString.wav';
+
+sound(soundWave2, fs);                          % Play the sound
+
+disp('Save file on disk...')                    % Save on disk
+audiowrite(fileName, soundWave2, fs);
---
+++ HL3/Delivery/PianoDelivery/homework_piano.m
@@ -0,0 +1,356 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Modeling of musical instruments homework.                               %
+% Numerical simulation of piano strings                                   %
+% Physical model for a struck string using finite difference.             %
+%                                                                         %
+% Musical Acoustics course                                                %
+% Ostan Paolo 10867276 
+% Donà Stefano 10867267
+% 2022                                                                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear
+close all
+clc
+
+if not(isfolder("plots"))
+    mkdir("plots")
+end
+
+%% Setup
+% define the string parameters and the simulation variables defined
+% according to the provided values and the numerical implementation.
+% We want to implement the finite difference scheme of a piano string tuned
+% as C2.
+
+% Temporal sampling parameters
+Fs = 16000;                     % [Hz]  Following the paper
+%Fs = 4*44100;                  % [Hz]  Following the slides
+
+T = 1/Fs;                       % [s]   single sample time length
+dur = 8;                        % [s]   Number of samples 
+N = dur*Fs;                     %       
+%t = linspace(0, Fs, N);
+
+
+% Boundary          
+
+z_l = 1e+20;
+z_b = 1000;
+
+% String parameters
+f_1 = 65.4;                     % [Hz]  Fundamental note
+str_L = 1.92;                   % [m]   string length
+M_s = 35e-3;                    % [Kg]  String mass
+%T_e = 750;                     % [N]   string tension given by the slides
+ 
+
+b_1 = 0.5;                      %       air damping coefficient
+b_2 = 6.25e-9;                  %       string internal friction coefficient
+epsilon = 7.5 * 10^(-6);        %       string stiffness parameter
+k = epsilon;                    %       string stiffness coefficient
+
+rho = M_s / str_L;              % [Kg/m]string linear density    
+
+T_e = 4*str_L^2*rho*f_1^2;      % [N]   string tension calculated  
+c = sqrt(T_e/rho);              % [m/s] string propagation velocity
+
+% Spatial sampling parameters
+%M = X = T * c       % maximum value which allows CFL conditions
+%M = 521;            % fixed by the slides
+
+
+
+% Aliasing condition
+
+
+% Number of maximum spatial steps
+gamma = Fs/(2*f_1);
+M_max = sqrt((-1+sqrt(1+16*epsilon*gamma^2))/(8*epsilon));
+% X_max = sqrt((1/2)*(c^2*T^2 + 4*b_2*T + sqrt(((c^2)*T^2 + 4*b_2*T)^2 + 16*k^2*T^2)));
+X_maxCFL = c/Fs;
+% M_max = str_L/X_max;
+
+% Spatial sampling
+
+M = floor(M_max); 
+% M = 123           % Value used to test stability, instable system if chosen
+X = str_L/M;
+
+% Integer values
+lambda = c*T/X;
+mu = k^2/(c^2*X^2);
+v= (2*b_2*T)/(X^2);
+
+
+
+
+
+
+% FD parameters
+a_1 = (-lambda^2*mu)/(1+b_1*T);
+a_2 = (lambda^2 + (4*lambda^2*mu) + v)/(1+b_1*T);
+a_3 = (2-2*lambda^2-6*lambda^2*mu-2*v)/(1+b_1*T);
+a_4 = (-1+b_1*T+2*v)/(1+b_1*T);
+a_5 = (-v)/(1+b_1*T);
+a_F = (T^2/rho)/(1+b_1*T);
+
+% Hammer parameters
+M_H = 4.9e-3;                   % [Kg]  mass of the hammer
+w = 0.2;                        %       width of the hammer spatial window 𝑔
+p = 2.3;
+Vh_0 = 2.5;                     % [m/s] initial hammer velocity
+b_H = 1e-4;                     % [1/s] fluid damping coefficient
+K = 4e+8;                       %       hammer felt stiffness
+a=0.12;                         %       x_0/L relative position of hammer strike
+a_M = floor(M*a);               %       relative hammer position in space samples
+
+% Hammer contact window definition
+w_M= round(w/X);
+g = hann(w_M);
+
+%PDE Coefficients:
+
+% Bridge boundary coefficients
+b_R1 = (2-2*lambda^2*mu-2*lambda^2)/(1+b_1*T+z_b*lambda);
+b_R2 = (4*lambda^2*mu+2*lambda^2)/(1+b_1*T+z_b*lambda);
+b_R3 = (-2*lambda^2*mu)/(1+b_1*T+z_b*lambda);
+b_R4 = (-1+b_1*T+z_b*lambda)/(1+b_1*T+z_b*lambda);
+b_RF = (T^2/rho)/(1+b_1*T+z_b*lambda);
+
+% Left hand (hinged string end) boundary coefficients
+b_L1 = (2-2*lambda^2*mu-2*lambda^2)/(1+b_1*T+z_l*lambda);
+b_L2 = (4*lambda^2*mu+2*lambda^2)/(1+b_1*T+z_l*lambda);
+b_L3 = (-2*lambda^2*mu)/(1+b_1*T+z_l*lambda);
+b_L4 = (-1+b_1*T+z_b*lambda)/(1+b_1*T+z_l*lambda);
+b_LF = (T^2/rho)/(1+b_1*T+z_l*lambda);
+
+% Hammer felt parameters
+
+d_1 = 2/(1+(b_H*T)/(2*M_H));
+d_2 =(-1 + (b_H*T)/(2*M_H))/(1 + (b_H*T)/(2*M_H));
+d_F = (-T^2/M_H)/(1+(b_H*T)/(2*M_H));
+
+
+%% Computation of the FD scheme
+% Initialization
+
+% y string displacement vector 
+y=zeros([N,M]);     % each row is a time instant considered. 
+                    % each column is a space fragment
+
+av_y = zeros(N,1);  % averaged displacement over 12 spatial samples
+
+% F_H time vector
+F_H=zeros(N,1);     % each row is a time instant considered. 
+                    % each column is a space fragment
+
+F=zeros(N,M);      % 
+
+% distribution of the spatial window on the vector
+space = linspace(0, str_L, M);
+j_start = a_M-(round(w_M/2));
+disp(j_start)
+for i=1:N
+    for j= 1:size(g)
+        F(i,(j)+j_start) = g(j); 
+
+    end
+
+end
+
+
+%% g(x,x0) force density
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+plot(space, F(10,:), LineWidth=1.2);
+grid minor;
+disp(F(10,a_M));
+xlabel("x [m]")
+ylabel("g ")
+title('$g(x,x_{0})$ shape and position of the force density function','interpreter','latex','FontSize',15)
+delete("./plots/ForceDensityShape.png");
+saveas(gcf, "./plots/ForceDensityShape.png");
+%%  LOOPS CALCULATIONS 
+
+% eta hammer displacement vector 
+eta = zeros(N,1);
+eta(1) = 0;         % eta time= 0
+eta(2) = Vh_0*T; 
+
+F_H(1) = K* abs(eta(1)-y(1,a_M))^p;
+F_H(2) = K* abs(eta(2)-y(2,a_M))^p;
+% Computation loop
+
+
+
+for in=2:N-1          %   time loop
+
+    
+   
+    for im=1:M      %   space loop
+
+        % solving displacement boundary conditions
+        switch im
+            % m=0
+            case 1
+                y(in+1,im) = b_L1*y(in,im)+b_L2*y(in,im+1)+b_L3*y(in,im+2)...
+                    +b_L4*y(in-1,im)+b_LF*F(in,im);
+            % m=1
+            case 2
+                y(in+1,im) = a_1*(y(in,im+2)-y(in,im)+2*y(in,im-1))+a_2*(y(in,im+1)+y(in,im-1))+ ...
+                    +a_3*(y(in,im))+a_4*(y(in-1,im))+a_5*(y(in-1,im+1)+y(in-1,im-1))+a_F*F(in,im);
+            % m=M-1
+            case M-1
+                y(in+1,im) = a_1*(2*y(in,im+1)-y(in,im)+y(in,im-2))+a_2*(y(in,im+1)+y(in,im-1))+ ...
+                    +a_3*(y(in,im))+a_4*(y(in-1,im))+a_5*(y(in-1,im+1)+y(in-1,im-1))+a_F*F(in,im);
+            % m=M
+            case M
+                y(in+1,im)= b_R1*y(in,im)+b_R2*y(in,im-1)+b_R3*y(in,im-2)+b_R4*y(in-1,im)+b_RF*F(in,im);
+            otherwise
+                y(in+1,im)= a_1*(y(in,im+2)+y(in,im-2))+a_2*(y(in,im+1)+y(in,im-1))+a_3*y(in,im)+a_4*(y(in-1,im))+a_5*(y(in-1,im+1)+y(in-1,im-1))+a_F*(F(in,im));
+        end
+    end
+
+    eta(in+1)= d_1*eta(in)+d_2*eta(in-1)+d_F*F_H(in);
+
+    if eta(in+1)>= y(in+1,a_M)
+            F_H(in+1) = K* abs(eta(in+1)-y(in+1,a_M))^p;
+            F(in+1,:)=F(in+1,:)*F_H(in+1);
+    else
+            F_H(in+1) =0;
+            F(in+1,:) = 0;
+    end
+
+    
+    for im=-5:6                     % Offset average over 12 samples
+        av_y(in,1) = av_y(in,1)+y(in,im+a_M);
+    end
+    av_y(in,1) = av_y(in,1)/12;
+    
+    
+end
+
+
+%% Plot the displacement of the whole string in time
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+x = linspace(0,1.92,M);
+for i = 1:(80*dur)
+    iTime = round(i*1/80*Fs);
+    grid minor;
+    plot(x,y(iTime,:),LineWidth=1.2);
+    ylim([-0.0005 0.0005])
+    grid on;
+    title(strcat('t = ',num2str(i*0.0125)));
+    ylabel("string displacement [m]");
+    xlabel("string extension [m]");
+    drawnow;
+    pause(1/20);
+end
+
+%% GIF creation
+close all;
+
+figure('Renderer', 'painters', 'Position', [100 100 800 500])
+filename = strcat("./plots/PianoStringGif_spatial_samples_",num2str(M),".gif");
+for i = 1:(40*dur)
+      iTime = round(i*1/40*Fs);
+      plot(x,y(iTime,:),LineWidth=1.2);
+      title(strcat('String Vertical displacement y, ',num2str(M) ," spatial samples"),'interpreter','latex','FontSize',15) 
+      ylabel("string displacement [m]");
+      xlabel("string extension [m]");
+      ylim([-0.0005 0.0005]);
+      drawnow
+      frame = getframe(1);
+      im = frame2im(frame);
+      [imind,cm] = rgb2ind(im,256);
+      if i == 1;
+          imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+      else
+          imwrite(imind,cm,filename,'gif','WriteMode','append','DelayTime',0.05);
+      end
+end
+
+%% Saving some samples of the string movement for report representation 
+upper = 8;
+lower = 0.001;
+numOfPics = 9;
+picTime= logspace(1,3,numOfPics);
+picTime_01 = picTime/max(picTime) - min(picTime)/max(picTime)
+picTime_08 = picTime_01*(upper-lower) + lower
+
+
+for i = 1:(numOfPics)
+    figure('Renderer', 'painters', 'Position', [100 100 800 500])
+    picTime_08Samp = round(picTime_08*Fs); 
+    grid minor;
+    plot(x,y(picTime_08Samp(i),:),LineWidth=1.8);
+    ylim([-0.0005 0.0005])
+    ylabel("string displacement [m]");
+    xlabel("string extension [m]");
+    grid on;
+    title(strcat('string Vertical displacement y at time $t = ',num2str(picTime_08(i)),"$,",num2str(M) ," spatial samples"),'interpreter','latex','FontSize',25) 
+    picName = strcat("./plots/PianoStringDispTime_",num2str(M),"_",num2str(i),".png");
+    delete(picName);
+    saveas(gcf, picName);
+end
+
+
+
+%% FFT of the averaged signal
+f=linspace(0,Fs,N);
+freqs = abs(fft(av_y(:,1)));
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+grid minor;
+plot(f,freqs, LineWidth=1.2);
+xlim([0,1200]);
+xline(65.4);
+ylabel("disp [m]")
+xlabel("Freqs [Hz]")
+text(65.4*(1.1), 0.65, strcat('f = ',num2str(65.4), " Hz"),'FontSize',15);
+title('Frequency Representation of the signal','interpreter','latex','FontSize',15)
+delete("./plots/FRFDisplacement.png");
+saveas(gcf, "./plots/FRFDisplacement.png");
+
+%% Plot the displacement in time
+t=linspace(0, dur, N);
+figure('Renderer', 'painters', 'Position', [100 100 1000 500])
+plot(t, av_y(:,1), LineWidth=1.2);
+grid minor
+xlabel("t [s]")
+ylabel("y [m]")
+title('Vertical displacement of the mean of 12 samples of the string around center hammering position','interpreter','latex','FontSize',15)
+delete("./plots/MeanDisplacement.png");
+saveas(gcf, "./plots/MeanDisplacement.png");
+
+
+%% Plot the synthesized signal play it and save it on the disk
+
+% Play the sound
+av_y_save = av_y./max(abs(av_y));
+sound(av_y_save,Fs)
+% pause(10);
+% sound(diff(av_y_save)*10,Fs)
+
+% Save on disk
+
+% requested filenames
+filenamePaolo = '10868276_Ostan_piano.wav';
+filenameStefano = "10868267_Donà_piano.wav";
+audiowrite(filenamePaolo,av_y_save,Fs);
+audiowrite(filenameStefano,av_y_save,Fs);
+
+
+%% Spectrogram of the audio signal 
+
+figure('Renderer', 'painters', 'Position', [100 100 800 600]);
+spectrogram(av_y_save,blackman(4096),64, 4096,Fs,"power");
+xlim([0,3])
+title('Spectrogram of the mean string displacement around hammer center position ','interpreter','latex','FontSize',15);
+xlabel("Freqs [kHz]")
+ylabel("Time [t]")
+xticks(0:0.0654:3)
+xticklabels(0:65.4:3000)
+% plot saving 
+delete("./plots/MeanDisplacementSpectrogram.png");
+saveas(gcf, "./plots/MeanDisplacementSpectrogram.png");
+
